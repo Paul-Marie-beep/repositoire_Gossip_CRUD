3ff6056015b72c725faa37ec937e5be77f27282c
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-05 14:32:17 +0200

    commit avant changement de nom gossip-potin

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,30 +2,24 @@ class GossipsController < ApplicationController
   
   
   def show
-    @gos = Potin.find(params[:id])
+    gossip_chosen
   end
 
   def index
-    @all_gossips = Potin.all
+    all_gossips
   end
 
   def new
         # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
    @new_gossip = Potin.new() #Cette ligne est important car, pour la première fois, elle fournit un nouveau potin sans erreur à la view, qui vérifie que c'est bien le cas.
-
-
   end
 
   def create
     # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
     # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
     # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
-    puts "$" * 60
-    puts "ceci est le contenu de params :"
-    puts params
-    puts "$" * 60
 
-    @all_gossips = Potin.all #On le remet pour que la view index, qui en a besoin, puisse afficher ce qu'elle doit afficher
+   all_gossips #On le remet pour que la view index, qui en a besoin, puisse afficher ce qu'elle doit afficher
 
     @new_gossip = Potin.new(title: params[:title],content: params[:content])
     @new_gossip.user = User.first
@@ -37,4 +31,25 @@ class GossipsController < ApplicationController
   end
 
 
+  def edit
+    gossip_chosen
+  end
+
+
+  def update
+
+  end
+
+  private
+
+  def gossip_chosen
+  @gos = Potin.find(params[:id])
+  end
+
+  def all_gossips
+    @all_gossips = Potin.all
+  end
+
+ 
 end
+
