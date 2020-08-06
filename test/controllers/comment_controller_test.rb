a5d3c090443b747bbf2bf06123fb596cd5386e24
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-06 23:09:40 +0200

    ???

--- test/controllers/comment_controller_test.rb
+++ test/controllers/comment_controller_test.rb
@@ -1,38 +1,28 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get user_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get user_show_url
-    assert_response :success
-  end
-
+class CommentControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get user_new_url
+    get comment_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get user_create_url
+    get comment_create_url
     assert_response :success
   end
 
   test "should get edit" do
-    get user_edit_url
+    get comment_edit_url
     assert_response :success
   end
 
   test "should get update" do
-    get user_update_url
+    get comment_update_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get user_destroy_url
+    get comment_destroy_url
     assert_response :success
   end
 
